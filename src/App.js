ac32c6aa73882102dbb39eb1587470162a378acd
katiewalker126 <katiewalker126@gmail.com>
2022-03-19 16:21:10 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -8,36 +8,38 @@ import Date from "./Date";
 export default function App() {
   return (
     <div className="App">
-      <div className="card">
-        <Search />
-        <div className="overview">
-          <div className="row">
-            <div className="col">
-              <div className="clearfix main-weather">
-                <Temperature />
-                <Image />
+      <div className="container">
+        <div className="card">
+          <Search />
+          <div className="overview">
+            <div className="row">
+              <div className="col">
+                <div className="clearfix main-weather">
+                  <Temperature />
+                  <Image />
+                </div>
+              </div>
+              <div className="col">
+                <Observations />
               </div>
             </div>
-            <div className="col">
-              <Observations />
+            <City />
+            <Date />
+          </div>
+          <footer>
+            <div className="footer-signature">
+              <a
+                href="https://github.com/katiewalker126/react-weather-app.git"
+                target="_blank"
+                rel="noreferrer"
+                className="github-link"
+              >
+                Open-source code
+              </a>{" "}
+              by Katie Walker
             </div>
-          </div>
-          <City />
-          <Date />
+          </footer>
         </div>
-        <footer>
-          <div className="footer-signature">
-            <a
-              href="https://github.com/katiewalker126/react-weather-app.git"
-              target="_blank"
-              rel="noreferrer"
-              className="github-link"
-            >
-              Open-source code
-            </a>{" "}
-            by Katie Walker
-          </div>
-        </footer>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import "./App.css";
 
